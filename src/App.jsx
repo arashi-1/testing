7344e82a320d7f75bed4b0175ead90be621589e7
arashi-1 <anoopkumar0935@gmail.com>
2024-11-08 14:51:43 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Chapter1 from "./utlis/pages/Chapter1";
 import Chapter2 from "./utlis/pages/Chapter2";
 import Layout from "./components/Layout";
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter basename="/testing">
       <Routes>
-        <Route path="/testing/" element={<Layout />} />
-        <Route path="/testing/chapter/1" element={<Chapter1 />} />
-        <Route path="/testing/chapter/2" element={<Chapter2 />} />
+        <Route path="/" element={<Layout />} />
+        <Route path="/chapter/1" element={<Chapter1 />} />
+        <Route path="/chapter/2" element={<Chapter2 />} />
       </Routes>
-    </>
+    </BrowserRouter>
   );
 };
 
