9c09ead7b682a75ca855f87a423770e1d6e15698
arashi-1 <anoopkumar0935@gmail.com>
2024-11-08 15:08:04 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import React from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Chapter1 from "./utlis/pages/Chapter1";
 import Chapter2 from "./utlis/pages/Chapter2";
 import Layout from "./components/Layout";
 
 const App = () => {
   return (
-    <BrowserRouter basename="/testing">
-      <Routes>
-        <Route path="/" element={<Layout />} />
-        <Route path="/chapter/1" element={<Chapter1 />} />
-        <Route path="/chapter/2" element={<Chapter2 />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />} />
+      <Route path="/chapter/1" element={<Chapter1 />} />
+      <Route path="/chapter/2" element={<Chapter2 />} />
+    </Routes>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
+// Wrap BrowserRouter with basename to set the base URL
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/testing">
+    {/* Set the base path for routing */}
     <App />
   </BrowserRouter>
 );
